31e7146f77d2f220ed569576bf76327994898f9e
Metin Türk <metinturk553@gmail.com>
2025-04-25 21:10:50 +0300

    İlk ödevimi bitirdim.

---
+++ Odev1.kt
@@ -0,0 +1,54 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    val sehir = "Eskisehir"
+    val ulke = "Turkiye"
+    val telefon = "5001112233"
+    val postaKodu = "26090"
+    val email = "metinturk553@gmail.com"
+    val meslek = "Bilgisayar Muh"
+    println(meslek)
+    var stokMiktari = 25
+    var musteriAdi = "Metin"
+    var bakiye = 1000
+    val dogumGunu = "01.Ocak.2005"
+    var maas = 50000
+    val medeniDurum = 'e' //(e == evli , b == bekar)
+    val urunYorum = true
+    var odemeTarihi = "01.Subat.2025"
+    var odeme = 850
+    var siparisAdeti = 2
+    val arabaModeli = "2012"
+    val kitapAdi = "Icimizdeki Şeytan"
+    var yayinlanmaTarihi = "01.Mart.2020"
+    var indirimMiktari = 0.1    //(%10 == 0.1)
+    var odaSayisi = 5
+    var enlem = 20
+    var boylam = 10
+    var urunadi = "Baklava"
+    var urunAdi = "Pilav"
+    var yemekFiyati = 250
+    val marka = "BMW"
+    val muzikAdi = "Rap"
+    var videoSuresi = 120 //dakika
+    var urunPuani = 5
+    var resimAdi = "Leonardo"
+    var dosyaFormati = ".zip"
+    var renk = "Mavi"
+    var renkKodu = "0FB65T30"
+    var telefonModeli = "11 Pro Max"
+    var ekranBoyutu = "11"//(inç)
+    var agirlik = 10 //(kg)
+    val ulusalGun = "23.Nisan"
+    val tatilGunu = "Pazar"
+    val rezarvasyonTarihi = "26.Nisan.2025"
+    val sokakAdi = "alicambaz"
+    val otobusHatti = "40T"
+    var kalanDakika = 20
+    val takipKodu = "ZQ345TR84O1903"
+    val kuponSuresi = 2 //(gun)
+    var kuponKodu = "BJK1903"
+    var faturaAdresi = "calikusu sok. no:20"
+
+
+}
